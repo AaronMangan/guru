da122161eec057c00790de2d84000ff7a1bf216b
Aaron Mangan <azza.mangan@gmail.com>
2026-04-22 15:35:22 +1000

    updated app name

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
             ignored: ['**/storage/framework/views/**'],
         },
         hmr: {
-          host: 'desk-oracle.localhost',
+          host: 'guru.localhost',
         },
     },
 });
