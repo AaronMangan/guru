bc2391265affc425fa9e716b5190deda67b5d104
Aaron Mangan <azza.mangan@gmail.com>
2026-04-22 16:13:12 +1000

    development

---
+++ database/migrations/2026_04_22_055222_add_relationships.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // User Relationships
+        // Desks Relationships
+        // Floor Relationships
+        // Statuses Relationships
+        // Teams Relationships
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
